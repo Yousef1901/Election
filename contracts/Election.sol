2fb7036f33195d968e56e8272e3f41cfd0a62cd2
yousef1901 <yousef.najjar1@gmail.com>
2018-11-09 20:35:10 +0100

    init

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.24;
+
+/**
+ * The contractName contract does this and that...
+ */
+contract Election {
+
+	string public candidate;
+
+	constructor () public {
+		
+		candidate = "Candidate 1";
+	}	
+}
