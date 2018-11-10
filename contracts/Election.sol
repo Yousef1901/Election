97c079972ea6b8a209d2046bf1340e5b48e7553c
najjar1 <yousef.najjar1@gmail.com>
2018-11-10 09:32:43 +0100

    change 1

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,11 +4,27 @@ pragma solidity ^0.4.24;
  * The contractName contract does this and that...
  */
 contract Election {
-
-	string public candidate;
+	//Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;	
+	}
+	
+	//store Candidates
+	//Fetch Candidate
+	mapping (uint => Candidate) public candidates;
+	//store Candidates Count
+	uint public candidatesCount;
 
 	constructor () public {
-		
-		candidate = "Candidate 1";
+		addCandidate("Candidate1");
+		addCandidate("Candidate2");
 	}	
+
+	function addCandidate (string _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+	
 }
